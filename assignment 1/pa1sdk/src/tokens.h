e2f2e43921b6e2633839d3073d09cd5f01e26e5b
mrqkc <mrqkc@mail.missouri.edu>
2014-02-19 14:01:43 -0600

    we are ballers

--- assignment 1/pa1sdk/src/tokens.h
+++ assignment 1/pa1sdk/src/tokens.h
@@ -6,7 +6,48 @@ enum {
       NUM,
       STR,
       VAR,
-/* More tokens go here. */
+      COMMA,
+      PERIOD,
+      COLON,
+      SEMICOLON,
+      LPAREN,
+      RPAREN,
+      LSQUARE,
+      RSQUARE,
+      LCURLY,
+      RCURLY,
+      PLUS,
+      MINUS,
+      DIVIDE,
+      MOD,
+      MULTIPLY,
+      OR, //|
+      AND, //&
+      LESSEQ, //<=
+	  GREATEREQ, //>=
+	  EQEQ, //==
+	  NOTEQ, //!=
+	  LESS, //<
+      GREATER, //>
+	  EQ, //=
+	  NOT, //!
+      IF,
+      COMMENT,
+      LMULTICOMMENT,
+	  RMULTICOMMENT,
+      TYPE,
+      INT,
+      BOOL,
+      ELSE,
+      FOR,
+      TO,
+      WHILE,
+      NIL,
+      TRU, //TRUE
+      FALS, //FALSE
+      MAIN,
+      FUNCTION,
+      RETURN,
 };
 
 extern char* yytext;
