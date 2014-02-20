0d3a8e4e27388a81df1f08181a00cf9e6263dcb9
Nate Thompson <nthompson55@gmail.com>
2014-02-19 18:30:12 -0600

    Main declared and tokens counted

--- assignment 1/pa1sdk/src/frontend.c
+++ assignment 1/pa1sdk/src/frontend.c
@@ -1,6 +1,52 @@
 #include "tokens.h"
 #include <stdio.h>
 
+extern int yylex(void);
+extern FILE *yyin;
+
+char *toknames[] = {"ID","NUM","STR","VAR","COMMA","PERIOD","COLON","SEMICOLON","LPAREN","RPAREN","LSQUARE",
+                    "RSQUARE","LCURLY","RCURLY","PLUS","MINUS","DIVIDE","MOD","MULTIPLY","OR","AND","LESSEQ","GREATEREQ", 
+                    "EQEQ","NOTEQ", "LESS","GREATER", "EQ","NOT","IF","COMMENT","LMULTICOMMENT","RMULTICOMMENT","TYPE","INT",
+                    "BOOL","ELSE","FOR","TO","WHILE","NIL","TRU", "FALS","MAIN","FUNCTION","RETURN"};
+
 int main(int argc, char** argv) {
+      char *fname;
+      int tok;
+      int numTok = 0;
+      int mainFlag = 0;
+      if(argc != 2)
+      {
+      	printf("Wrong number of files!");
+      	return 0;
+      }
+
+      fname = argv[1];
+      yyin = fopen(fname,"r");
+      if(yyin == NULL)
+      {
+      	printf("file unreadable");
+      	return 0;
+      }
+      
+      
+      for(;;)
+      {
+            tok = yylex();
+            if(tok==0)
+                  break;
+            numTok++;
+            switch(tok)
+            {
+                  case MAIN:
+                        {
+                              mainFlag = 1;
+                              break;
+                        }
+            }
+
+      }
+
+      printf("Total tokens: %d\n",numTok);
+      printf("Main Declared: %s\n",mainFlag?"yes":"no");
       return 0;
 }
