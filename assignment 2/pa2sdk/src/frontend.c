124954817f94e5147643441e41ef83931d996051
Nathaniel Thompson <nthompson55@gmail.com>
2014-03-13 21:12:20 -0500

    partial changes

--- assignment 2/pa2sdk/src/frontend.c
+++ assignment 2/pa2sdk/src/frontend.c
@@ -2,5 +2,22 @@
 #include "parser.h" // yyparse, yylval, token enum, etc.
 
 int main(int argc, char** argv) {
-      return 0;
+ //Check for correct number of files    
+ if(argc != 2)
+ {
+   printf("Error, incorrect usage, pa2 <FILE.lig>\n");
+   return 0;
+ }
+
+ //throw an error if file is unreadable
+ char *fname = NULL;
+ fname = argv[1];
+ yyin = fopen(fname,"r");
+ if(yyin == NULL)
+ {
+   printf("file unreadable");
+   return 0;
+ }
+ yyparse();
+return 0;
 }
